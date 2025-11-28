5996d74ed73aab9a9b0a74a67efdf8596a442dc0
haxsysgit <elenasuluarinze@gmail.com>
2025-11-28 14:44:57 +0100

    refactor: simplify routes - remove auth and dashboard views

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Classes from '../views/Classes.vue'
-import ClassDetail from '../views/ClassDetail.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Lessons from '../views/Lessons.vue'
+import LessonDetail from '../views/LessonDetail.vue'
 import Cart from '../views/Cart.vue'
 import Checkout from '../views/Checkout.vue'
-import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
   {
@@ -15,25 +12,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/classes',
-    name: 'Classes',
-    component: Classes
+    path: '/lessons',
+    name: 'Lessons',
+    component: Lessons
   },
   {
-    path: '/class/:id',
-    name: 'ClassDetail',
-    component: ClassDetail,
+    path: '/lesson/:id',
+    name: 'LessonDetail',
+    component: LessonDetail,
     props: true
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register
   },
   {
     path: '/cart',
@@ -44,11 +31,6 @@ const routes = [
     path: '/checkout',
     name: 'Checkout',
     component: Checkout
-  },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
   }
 ]
 
